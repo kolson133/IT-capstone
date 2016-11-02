ef2bbf4c06a7b29d44f9cf6e6a87e929b3d765d0
Austin Prueher <aprueher6@student.cvtc.edu>
2016-11-02 17:29:21 -0500

    basic html structure

--- index.php
+++ index.php
@@ -2,12 +2,36 @@
 <html>
     <head>
         <meta charset="UTF-8">
-        <title></title>
-        <link rel="stylesheet" href="css/styles.css?v=1.0">
+        <title>Don't Worry, Bar Happy!</title>
+        <link rel="stylesheet" href="css/styles.css">
     </head>
     <body>
+        <div id="wrapper">
+            <header>
+                <h1>Don't Worry</h1>
+                <h1>Bar Happy!</h1>
+            </header>
+            <nav>
+                <ul>
+                    <li>Search</li>
+                    <li>Contact Us</li>
+                </ul>
+            </nav>
+            <main>
+                <div id="outerSearchBox">
+                    <ul id="searchTypes">
+                        <li>By Zip</li>
+                        <li>By City</li>
+                        <li>By Name</li>
+                    </ul>
+                    <div id="innerSearchBox">
+                        
+                    </div>
+                </div>
+            </main>
         <?php
-        // put your code here
+        
         ?>
+        </div>
     </body>
 </html>
