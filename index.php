eb3a60339c5976ecab05be406eb9e75cbca0e099
Austin Prueher <aprueher6@student.cvtc.edu>
2016-11-02 17:19:43 -0500

    Initial php commit

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+        <link rel="stylesheet" href="css/styles.css?v=1.0">
+    </head>
+    <body>
+        <?php
+        // put your code here
+        ?>
+    </body>
+</html>
