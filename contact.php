d20fef0cf6c3c9480f793884bbb31fa36d07b4a9
Kevin Olson <achilles2865@gmail.com>
2016-11-12 03:00:26 -0600

    contact updates

---
+++ contact.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title>Dont' Worry, Bar Happy</title>
+        <meta name="BarHappy" content="contactUs">
+        <link rel="stylesheet" href="css/styles.css">
+    </head>
+    
+    <body>     
+        <h1 class="logo">Don't Worry, Bar Happy!</h1>
+        <div class="contactInfo">
+             <p>This is all filler content to allow me to see what I need to do for the css work
+            I can make any changes later. I just need more text to see the size of my div. Now I
+            will copy this text to get the height. ******* This is all filler content to allow me to see what I need to do for the css work
+            I can make any changes later. I just need more text to see the size of my div. Now I
+            will copy this text to get the height. ********* This is all filler content to allow me to see what I need to do for the css work
+            I can make any changes later. I just need more text to see the size of my div. Now I
+            will copy this text to get the height.This is all filler content to allow me to see what I need to do for the css work
+            I can make any changes later. I just need more text to see the size of my div. Now I
+            will copy this text to get the height. ******* This is all filler content to allow me to see what I need to do for the css work
+            I can make any changes later. I just need more text to see the size of my div. Now I
+            will copy this text to get the height. ********* This is all filler content to allow me to see what I need to do for the css work
+            I can make any changes later. I just need more text to see the size of my div. Now I
+            will copy this text to get the height.This is all filler content to allow me to see what I need to do for the css work
+            I can make any changes later. I just need more text to see the size of my div. Now I
+            will copy this text to get the height. ******* This is all filler content to allow me to see what I need to do for the css work
+            I can make any changes later. I just need more text to see the size of my div. Now I
+            will copy this text to get the height. ********* This is all filler content to allow me to see what I need to do for the css work
+            I can make any changes later. I just need more text to see the size of my div. Now I
+            will copy this text to get the height.This is all filler content to allow me to see what I need to do for the css work
+            I can make any changes later. I just need more text to see the size of my div. Now I
+            will copy this text to get the height. ******* This is all filler content to allow me to see what I need to do for the css work
+            I can make any changes later. I just need more text to see the size of my div. Now I
+            will copy this text to get the height. ********* This is all filler content to allow me to see what I need to do for the css work
+            I can make any changes later. I just need more text to see the size of my div. Now I
+            will copy this text to get the height.</p>
+        </div><br><br><br><br><br><br>
+        
+        <?php
+        
+        ?>
+        
+        <footer>
+            <div class="nav"></div>
+                <ul>
+                    <li><a href="contact.php">Contact Us</a></li>
+                    <li><a href="index.php">Home</a></li>
+                    <li><a href="#">Submit</a></li>
+                </ul>
+            </div>
+        </footer>
+    </body>
+</html>
