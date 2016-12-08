f80cd8e847e8c9201cb959081b634e62b141bfbf
Jasonm1066 <jasonm1066@gmail.com>
2016-12-08 13:07:47 -0600

    Add files via upload

---
+++ yelp.php
@@ -0,0 +1,156 @@
+#!/usr/bin/php
+<?php
+/**
+ * Yelp API v2.0 code sample.
+ *
+ * This program demonstrates the capability of the Yelp API version 2.0
+ * by using the Search API to query for businesses by a search term and location,
+ * and the Business API to query additional information about the top result
+ * from the search query.
+ * 
+ * Please refer to http://www.yelp.com/developers/documentation for the API documentation.
+ * 
+ * This program requires a PHP OAuth2 library, which is included in this branch and can be
+ * found here:
+ *      http://oauth.googlecode.com/svn/code/php/
+ * 
+ * Sample usage of the program:
+ * `php sample.php --term="bars" --location="San Francisco, CA"`
+ */
+// Enter the path that the oauth library is in relation to the php file
+require_once('OAuth.php');
+// Set your OAuth credentials here  
+// These credentials can be obtained from the 'Manage API Access' page in the
+// developers documentation (http://www.yelp.com/developers)
+$CONSUMER_KEY = 'G9wSqAYtCMWwXVGPdHcaHw';
+$CONSUMER_SECRET = 'GHkmbFlmsdJ8hoWhvNNFDrknEKg';
+$TOKEN = 'v4Q0Q2Rbd_aJ643L-Y0MwTz1tnb66mst';
+$TOKEN_SECRET = 'E0ZXf_1WSROqEkK_iYjFFP_kNpM';
+$API_HOST = 'api.yelp.com';
+$DEFAULT_TERM = 'bar';
+$DEFAULT_LOCATION = 'Eau Claire, WI';
+$SEARCH_LIMIT = 7;
+$SEARCH_PATH = '/v2/search/';
+$BUSINESS_PATH = '/v2/business/';
+/** 
+ * Makes a request to the Yelp API and returns the response
+ * 
+ * @param    $host    The domain host of the API 
+ * @param    $path    The path of the APi after the domain
+ * @return   The JSON response from the request      
+ */
+function request($host, $path) {
+    $unsigned_url = "https://" . $host . $path;
+    // Token object built using the OAuth library
+    $token = new OAuthToken($GLOBALS['TOKEN'], $GLOBALS['TOKEN_SECRET']);
+    // Consumer object built using the OAuth library
+    $consumer = new OAuthConsumer($GLOBALS['CONSUMER_KEY'], $GLOBALS['CONSUMER_SECRET']);
+    // Yelp uses HMAC SHA1 encoding
+    $signature_method = new OAuthSignatureMethod_HMAC_SHA1();
+    $oauthrequest = OAuthRequest::from_consumer_and_token(
+        $consumer, 
+        $token, 
+        'GET', 
+        $unsigned_url
+    );
+    
+    // Sign the request
+    $oauthrequest->sign_request($signature_method, $consumer, $token);
+    
+    // Get the signed URL
+    $signed_url = $oauthrequest->to_url();
+    
+    // Send Yelp API Call
+    try {
+        $ch = curl_init($signed_url);
+        if (FALSE === $ch)
+            throw new Exception('Failed to initialize');
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_HEADER, 0);
+        $data = curl_exec($ch);
+        if (FALSE === $data)
+            throw new Exception(curl_error($ch), curl_errno($ch));
+        $http_status = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+        if (200 != $http_status)
+            throw new Exception($data, $http_status);
+        curl_close($ch);
+    } catch(Exception $e) {
+        trigger_error(sprintf(
+            'Curl failed with error #%d: %s',
+            $e->getCode(), $e->getMessage()),
+            E_USER_ERROR);
+    }
+    
+    return $data;
+}
+/**
+ * Query the Search API by a search term and location 
+ * 
+ * @param    $term        The search term passed to the API 
+ * @param    $location    The search location passed to the API 
+ * @return   The JSON response from the request 
+ */
+function search($term, $location) {
+    $url_params = array();
+    
+    $url_params['term'] = $term ?: $GLOBALS['DEFAULT_TERM'];
+    $url_params['location'] = $location?: $GLOBALS['DEFAULT_LOCATION'];
+    $url_params['limit'] = $GLOBALS['SEARCH_LIMIT'];
+    $search_path = $GLOBALS['SEARCH_PATH'] . "?" . http_build_query($url_params);
+    
+    return request($GLOBALS['API_HOST'], $search_path);
+}
+/**
+ * Query the Business API by business_id
+ * 
+ * @param    $business_id    The ID of the business to query
+ * @return   The JSON response from the request 
+ */
+function get_business($business_id) {
+    $business_path = $GLOBALS['BUSINESS_PATH'] . urlencode($business_id);
+    
+    return request($GLOBALS['API_HOST'], $business_path);
+}
+/**
+ * Queries the API by the input values from the user 
+ * 
+ * @param    $term        The search term to query
+ * @param    $location    The location of the business to query
+ */
+function query_api($term, $location) {     
+    
+    
+    for ($x = 0; $x < 7; $x++) {
+        $response = json_decode(search($term, $location));
+
+        $name = $response->businesses[$x]->name;
+        $telephone = $response->businesses[$x]->phone;
+        
+        $business_id = $response->businesses[$x]->id;
+        
+
+        // TODO: The below 4 lines are just for debugging. They along with the other comments can be removed before launch.
+        
+        echo "<pre>";
+        var_dump($response);
+        echo "</pre>";
+        echo "$name also known as  $business_id on Yelp can be reached at  $telephone";
+
+
+
+    }
+}
+/**
+ * User input is handled here 
+ */
+
+$longopts  = array(
+    "term::",
+    "location::",
+);
+    
+$options = getopt("", $longopts);
+$term = $options['term'] ?: '';
+$location = $options['location'] ?: $_GET['zip'];
+query_api($term, $location);
+?>
